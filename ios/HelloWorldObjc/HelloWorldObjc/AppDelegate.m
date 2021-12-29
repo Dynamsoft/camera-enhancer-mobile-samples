fc713ef496e0f05b1f0bdf74d64ee54bd16bddc8
Henry-Song021 <78675724+Henry-Song021@users.noreply.github.com>
2021-12-29 15:20:33 +0800

    Updates from develop

--- ios/HelloWorldObjc/HelloWorldObjc/AppDelegate.m
+++ ios/HelloWorldObjc/HelloWorldObjc/AppDelegate.m
@@ -1,5 +1,6 @@
 
 #import "AppDelegate.h"
+#import <DynamsoftCameraEnhancer/DynamsoftCameraEnhancer.h>
 
 @interface AppDelegate ()
 
@@ -9,6 +10,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     // The string "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9" here will grant you a public trial license good for 7 days. After that, please visit: https://www.dynamsoft.com/customer/license/trialLicense?product=dce&utm_source=installer&package=ios to request for 30 days extension.
     [DynamsoftCameraEnhancer initLicense:@"DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9" verificationDelegate:self];
     // Override point for customization after application launch.
--- ios/HelloWorldSwift/HelloWorldSwift/AppDelegate.swift
+++ ios/HelloWorldSwift/HelloWorldSwift/AppDelegate.swift
@@ -5,6 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // The string "DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9" here will grant you a public trial license good for 7 days. After that, please visit: https://www.dynamsoft.com/customer/license/trialLicense?product=dce&utm_source=installer&package=ios to request for 30 days extension.
         DynamsoftCameraEnhancer.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", verificationDelegate: self)
         // Override point for customization after application launch.
